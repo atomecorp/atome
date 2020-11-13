81e656f289a9b84197101b6084c8401549a97274
jean-ericgodard <jeezs@jeezs.net>
2020-11-13 16:26:03 +0100

    whole new clearer file architecture

--- helper/default_methods_for_atome_api.rb
+++ helper/default_methods_for_atome_api.rb
@@ -14,7 +14,7 @@ end
 class Sparkle
   def initialize
     # the line below create atomes's methods using meta-programming
-    atome_methods = %i[atome_id toto]
+    atome_methods = %i[atome_id color]
     atome_methods.each do |method_name|
       Atome.define_method method_name do |params = nil, &proc|
         # this is the main entry method for the current property treatment
@@ -29,6 +29,7 @@ class Sparkle
         method_analysis params, instance_method_name, method_name, proc if params || proc
         # finally we return the current property using magic_getter
         unless params
+          # no params send we call the getter
           magic_getter instance_method_name
         end
       end
@@ -42,67 +43,7 @@ class Sparkle
 end
 # the class below contains all common properties's methods
 module Properties
-  # the method below must be executed once only
-
-  def reformat_params(params, method_name, proc)
-    # this method allow user to input simple data and reformat the incoming data so it always store a Hash
-    # if its a string then the data is put into the property content
-    #   ex with color property : {color: {content: :red}}
-    # if it's an array then property itself change from Hash type to array
-    #   ex with color property{color: [{content: :red}{content: :yellow, x: 200}]}
-
-    if params.instance_of?(Array)
-
-    else
-      params
-
-    end
-
-    #params |= {}
-    #
-    #if params.instance_of?(Hash)
-    #  params[:proc] = proc if proc
-    #  #params
-    #elsif params.instance_of?(Array)
-    #  params.each do |param|
-    #    if param.instance_of?(Hash)
-    #      param[:add] = true
-    #      send(method_name, param)
-    #    else
-    #      send(method_name, {content: param, add: true})
-    #    end
-    #    #return false
-    #  end
-    #  send(method_name, {proc: proc, add: true}) if proc
-    #  {}
-    #elsif proc
-    #  params[:proc]=proc
-    #  #{content: params, proc: proc}
-    #else
-    #  {content: params}
-    #end
-    #params
-    #unless params
-    #  params={}
-    #end
-    #if params.instance_of?(Array)
-    #elsif params.instance_of?(String) || params.instance_of?(Symbol)
-    #  params={content: params}
-    #end
-    #if proc
-    #  puts "message is #{params.class} Location: default_methods_for_atome_api.rb, line 79"
-    #  params[:proc]=proc
-    #end
-
-    #if params && proc
-    #elsif params
-    #elsif proc
-    #
-    #else
-    #  nil
-    #
-    #end
-  end
+  # the methods below must be executed once only
 
   def send_hash(params, method_name)
     # if prop needs to be refresh we send it to the Render engine
@@ -117,163 +58,35 @@ module Properties
     elsif params.instance_of?(Array)
       # if params is an array we send each item of the array to 'found_property_parsing' as a Hash
       params.each do |param|
-        send(method_name.to_s + "_parsing", param)
+        property_parsing param, method_name
       end
     end
   end
 
-  #def method_analysis(params, instance_variable, method_name, proc)
-  #  # this method first send the params to the 'reformat_params' method to ensure it return a hash or an array
-  #  params = reformat_params params, method_name, proc
-  #  # then either it parse the params if {add: true} is found, so it add the params to the property hash,
-  #  # either it add any {key: :value} found to the instance variable property
-  #  if params[:add] && params[:add] == true
-  #    # this method is called when the property hash contain {add: true}
-  #    # then hash property is turn into an array so it can accept many times the same property.
-  #    # use for gradient or multiple shadows, etc...  ex : color: [{content : red, x: 0},{content : pink, x: 200}]
-  #    #params.delete(:add)
-  #    previous_params = instance_variable_get("@#{method_name}")
-  #    if previous_params.empty?
-  #      instance_variable_set("@#{method_name}", [params])
-  #    elsif previous_params.instance_of?(Hash)
-  #      instance_variable_set("@#{method_name}", [previous_params, params])
-  #    elsif previous_params.instance_of?(Array)
-  #      instance_variable_set("@#{method_name}", previous_params << params)
-  #    end
-  #  #elsif instance_variable.instance_of?(Array) || params[:content]
-  #    #params.delete(:add)
-  #    #puts "************11*************"
-  #    #
-  #    #instance_variable_set("@#{method_name}", params)
-  #  #elsif params[:content]
-  #  #  puts "***********22**************"
-  #    #params.delete(:add)
-  #    #params.each do |key, value|
-  #    #  instance_variable[key] = value
-  #    #end
-  #    instance_variable_set("@#{method_name}", params)
-  #  elsif !params[:content]
-  #    #puts "############000############"
-  #
-  #    #params.delete(:add)
-  #    #params.delete(:content)
-  #    #instance_variable.delete(:content)
-  #    #instance_variable_set("@#{method_name}", params)
-  #    params.each do |key, value|
-  #      instance_variable[key] = value
-  #    end
-  #  else
-  #    #puts "************11*************"
-  #
-  #    instance_variable_set("@#{method_name}", params)
-  #  end
-  #  #puts "#{params.class}"
-  #  params.delete(:add)
-  #  #instance_variable.delete(:add)
-  #  # now we apply the specific treatment according to the property found if the hash is not empty
-  #  property_parsing(params, method_name) if params != {}
-  #end
+  def recursive_treatment(params, instance_variable, method_name, proc)
+    params.each do |param|
+      params[:add] = true if params.instance_of?(Hash)
+      method_analysis param, instance_variable, method_name, proc
+    end
+  end
 
-  # original version below
-  #def method_analysis(params, instance_variable, method_name, proc)
-  #  # this method first send the params to the 'reformat_params' method to ensure it return a hash or an array
-  #  params = reformat_params params, method_name, proc
-  #  # then either it parse the params if {add: true} is found, so it add the params to the property hash,
-  #  # either it add any {key: :value} found to the instance variable property
-  #  if params[:add] && params[:add] == true
-  #    # this method is called when the property hash contain {add: true}
-  #    # then hash property is turn into an array so it can accept many times the same property.
-  #    # use for gradient or multiple shadows, etc...  ex : color: [{content : red, x: 0},{content : pink, x: 200}]
-  #    #params.delete(:add)
-  #    previous_params = instance_variable_get("@#{method_name}")
-  #    if previous_params.empty?
-  #      instance_variable_set("@#{method_name}", [params])
-  #    elsif previous_params.instance_of?(Hash)
-  #      instance_variable_set("@#{method_name}", [previous_params, params])
-  #    elsif previous_params.instance_of?(Array)
-  #      instance_variable_set("@#{method_name}", previous_params << params)
-  #    end
-  #  elsif instance_variable.instance_of?(Array)
-  #    #params.delete(:add)
-  #    instance_variable_set("@#{method_name}", params)
-  #  elsif params[:content]
-  #    #params.delete(:add)
-  #    params.each do |key, value|
-  #      instance_variable[key] = value
-  #    end
-  #  else
-  #    #params.delete(:add)
-  #    #params.delete(:content)
-  #    #instance_variable.delete(:content)
-  #    #params[:content]="#############"
-  #    #instance_variable_set("@#{method_name}", params)
-  #    params.each do |key, value|
-  #      instance_variable[key] = value
-  #    end
-  #  end
-  #  #instance_variable.delete(:add)
-  #  # now we apply the specific treatment according to the property found if the hash is not empty
-  #  property_parsing(params, method_name) if params != {}
-  #end
+  def property_save(params, instance_variable, method_name)
+    #puts params[:add]
+     instance_variable = params
+     puts "message :#{instance_variable} from : default_methods_for_atome_api.rb : 76"
+  end
 
   def method_analysis(params, instance_variable, method_name, proc)
-    add = nil
-    #if params.instance_of?(Array)
-    #  params.each do |property|
-    #    #puts "line 189 : #{property}"
-    #    #add=true if property[:add]
-    #  end
-    #  #puts params.class
-    #else
-    #  #add=true if params[:add]
-    #end
-    #puts "###### if  #{add} parse content ######"
-
-    # this method first send the params to the 'reformat_params' method to ensure it return a hash or an array
-    #puts "#########"
-    #puts "pre  : #{params} : #{params.class}, proc : #{proc}"
-    puts "#########"
-
-    #puts "pre  #{params.class},"
-
-    params = reformat_params params, method_name, proc
-    puts "post : #{params} "
-    ## then either it parse the params if {add: true} is found, so it add the params to the property hash,
-    ## either it add any {key: :value} found to the instance variable property
-    #if add
-    #  # this method is called when the property hash contain {add: true}
-    #  # then hash property is turn into an array so it can accept many times the same property.
-    #  # use for gradient or multiple shadows, etc...  ex : color: [{content : red, x: 0},{content : pink, x: 200}]
-    #  #params.delete(:add)
-    #  previous_params = instance_variable_get("@#{method_name}")
-    #  if previous_params.empty?
-    #    instance_variable_set("@#{method_name}", [params])
-    #  elsif previous_params.instance_of?(Hash)
-    #    instance_variable_set("@#{method_name}", [previous_params, params])
-    #  elsif previous_params.instance_of?(Array)
-    #    instance_variable_set("@#{method_name}", previous_params << params)
-    #  end
-    #elsif instance_variable.instance_of?(Array)
-    #  #params.delete(:add)
-    #  instance_variable_set("@#{method_name}", params)
-    #elsif params[:content]
-    #  #params.delete(:add)
-    #  params.each do |key, value|
-    #    instance_variable[key] = value
-    #  end
-    #else
-    #  #params.delete(:add)
-    #  #params.delete(:content)
-    #  #instance_variable.delete(:content)
-    #  #params[:content]="#############"
-    #  #instance_variable_set("@#{method_name}", params)
-    #  params.each do |key, value|
-    #    instance_variable[key] = value
-    #  end
-    #end
-    ##instance_variable.delete(:add)
-    ## now we apply the specific treatment according to the property found if the hash is not empty
-    #property_parsing(params, method_name) if params != {}
+    if params.instance_of?(Array)
+      recursive_treatment params, instance_variable, method_name, proc
+    else
+      if params && !params.instance_of?(Hash)
+        params = {content: params}
+      end
+      params ||= {}
+      params[:proc] = proc if proc
+      property_save(params, instance_variable, method_name)
+    end
   end
 
   def magic_getter(method)
@@ -292,7 +105,7 @@ module Singularity
 
   include Properties
 
-  def toto_dsp(params)
+  def color_dsp(params)
     "toto dsp :  #{params}"
   end
 
@@ -359,24 +172,26 @@ end
 # ------- verification 2 -------
 Sparkle.new
 a = Atome.new
-#a.toto(:my_data)
-#a.toto(:tutu)
-#a.toto(:ok_for_now)
-#a.toto({content: :doto, kool: :ok})
-#a.toto({content: :didi, kool: :pas_ok, refresh: false})
-#a.toto([{content: :datos, x: 0}, :tomoldu])
-#a.toto(:dudu)
-#a.toto({content: :titi, kool: :ok2, add: true})
-#a.toto(:mimi)
-a.toto([{content: :ditos, x: 0, add: false}, :toti]) do
-  puts "it works"
-end
-#a.toto({add: true, content: :conteni})
-#a.toto({add: false}) do
+#a.color(:my_data)
+#a.color(:tutu)
+#a.color(:ok_for_now)
+#a.color({content: :doto, kool: :ok})
+#a.color({content: :didi, kool: :pas_ok, refresh: false})
+a.color([{content: :datos, x: 0}, :tomoldu])
+puts a.color
+#a.color(:dudu)
+#a.color({content: :titi, kool: :ok2, add: true})
+#a.color(:mimi)
+#a.color([{content: :ditos, x: 0, add: false}, :toti]) do
+#  puts "it works"
+#end
+#a.color()
+#a.color({add: true, content: :conteni})
+#a.color({add: false}) do
 #  puts "it's cool"
 #end
 #
-#a.toto() do
+#a.color() do
 #  puts "it's cool"
 #end
 
