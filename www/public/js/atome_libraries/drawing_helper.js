56718c313ee59185f8c1df3eaf76f69f6b87e551
Régis Ramillien <r.ramillien@tritonit.tech>
2021-02-10 22:27:01 +0100

    Added POC on drawing bezier curve (squiggle) with ZIM.

--- www/public/js/atome_libraries/drawing_helper.js
+++ www/public/js/atome_libraries/drawing_helper.js
@@ -33,13 +33,29 @@ class DrawingHelper {
         this.frame.on("ready", () => {
             const stage = this.frame.stage;
 
-            const circle = new zim.Circle(50, green).loc(200, 200);
-            stage.update();
+            // const circle = new zim.Circle(50, green).loc(200, 200);
+            // stage.update();
+
+            var points = [];
+            const squiggle = new Squiggle({
+                color: blue,
+                thickness: 4
+            });
+            var init = false;
 
             function beginDraw(x, y) {
-                circle
-                    .top()
-                    .animate({x, y}, 0.5);
+                // circle
+                //     .top()
+                //     .animate({x, y}, 0.5);
+
+                points.push([x, y, 0, 0, -30, 0, 30, 0, "mirror"]);
+                squiggle.points = points;
+                if(!init) {
+                    squiggle.addTo();
+                    // var tm = new TransformManager([squiggle]);
+                    init = true;
+                }
+
                 stage.update();
             }
 
