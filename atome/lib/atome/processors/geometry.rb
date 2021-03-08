4b5e8ba18517b9335be37ffd0a705a44bd7c5f35
jean-ericgodard <jeezs@jeezs.net>
2021-03-08 12:27:19 +0100

    method size is debug when using no params or no bloc

--- atome/lib/atome/processors/geometry.rb
+++ atome/lib/atome/processors/geometry.rb
@@ -1,20 +1,26 @@
 module Processors
   def size_processor(value)
-    size = 0
-    if width && height
-      size = value[:value]
-      if width > height
-        ratio = size / width
-        self.width = size
-        self.height = height * ratio
-      else
-        ratio = size / height
-        self.height = size
-        self.width = width * ratio
-      end
+    if value.instance_of?(Hash) && value[:value].nil?
     else
-      self.height = size
-      self.width = size
+      size = 0
+      if value.instance_of?(Number) || value.instance_of?(Integer)
+        value = {value: value}
+      end
+      if width && height
+        size = value[:value]
+        if width > height
+          ratio = size / width
+          self.width = size
+          self.height = height * ratio
+        else
+          ratio = size / height
+          self.height = size
+          self.width = width * ratio
+        end
+      else
+        self.height = size
+        self.width = size
+      end
     end
   end
 end
