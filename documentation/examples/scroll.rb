ad87b0770816f33ccf6583786c3384f9bb2cce17
jean-ericgodard <jeezs@jeezs.net>
2021-03-08 10:18:34 +0100

    add position for atome with example

--- documentation/examples/scroll.rb
+++ documentation/examples/scroll.rb
@@ -1,8 +1,8 @@
 # Scroll
 
 t = text({content: "drag the circle out the window",width: 396, x: 66, position: :fixed})
-c = circle({x: 77})
-c.y(96).x(96).drag(true)
+c = circle({x: 96, y: 96})
+c.drag(true)
 
 grab(:view).scroll_html do |evt|
   t.content("x position : #{c.x}, y position : #{c.y}")
