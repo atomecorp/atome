3cc674dc7d265b57f6f943c971b8f62cdb8320d8
Jean-Eric Godard <jeezs@jeezs.net>
2021-03-09 15:09:26 +0100

    Delete atome/lib/atome/generated_properties directory

--- atome/lib/atome/generated_properties/atome_methods.rb
+++
@@ -1,5 +0,0 @@
-module AtomeHelpers
-  def methods
-    [:atome_id, :audio, :blur, :border, :box, :can, :center, :child, :circle, :color, :content, :display, :drag, :edit, :enliven, :example, :height, :id, :image, :info, :insert, :language, :monitor, :opacity, :over, :overflow, :parent, :position, :preset, :private, :receive, :record, :render, :rotate, :selector, :shadow, :share, :size, :smooth, :tactile, :text, :touch, :transmit, :type, :video, :width, :x, :xx, :y, :yy, :z]
-  end
-end
