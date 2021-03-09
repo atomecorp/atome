e1822741fe9e0428754a6e2df22f57624a31e477
jean-ericgodard <jeezs@jeezs.net>
2021-03-09 19:17:07 +0100

    First and basic implementation of midi in atome Added option on touch (up down long) long needs debug it can triger the event many times

--- atome/lib/atome/processors/communication.rb
+++ atome/lib/atome/processors/communication.rb
@@ -1,22 +1,8 @@
 #module Processors
-#  def transmit(value)
-#    case value[:protocole]
-#    when :midi
-#      self.midi_communication(value[:content])
-#    when :beaglebone
-#      # bbb communication layer
-#    else
-#      value
-#    end
-#  end
-#end
-
 module Processors
   def transmit(values)
     case values.keys[0]
     when :midi
-      default = {note: 39, channel: 1}
-      #alert values[:midi]
       case values[:midi].keys[0]
       when :play
         note = values[:midi][:play].delete(:note)
@@ -28,15 +14,12 @@ module Processors
         channel = values[:midi][:stop].delete(:channel)
         options = values[:midi][:stop]
         midi_stop(note, channel, options)
-
       when :inputs
         #alert :kool
-
       when :outputs
-
       else
+        values
       end
-
     when :beaglebone
       # bbb communication layer
     else
