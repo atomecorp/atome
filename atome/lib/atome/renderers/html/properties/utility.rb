cdb5b4cd70ad170abcad8a02ef6e582797511208
jean-ericgodard <jeezs@jeezs.net>
2021-04-10 12:41:08 +0200

    Add the capture methods first incarnation of the record anything on screen to script!!

--- atome/lib/atome/renderers/html/properties/utility.rb
+++ atome/lib/atome/renderers/html/properties/utility.rb
@@ -42,4 +42,15 @@ module PropertyHtml
       content(content, false)
     end
   end
+
+  def record_html(params)
+    if params == :stop
+      jq_get(atome_id).unbind(:mousemove)
+    else
+      jq_get(atome_id).on(:mousemove) do |evt|
+        proc = params[:proc]
+        proc.call(evt) if proc.is_a?(Proc)
+      end
+    end
+  end
 end
