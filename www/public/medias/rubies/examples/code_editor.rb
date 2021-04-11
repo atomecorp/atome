d3a5b8521db5fcb4c00e15eaf785d7675b3a98e7
lesylvs <lesylvs@mac.com>
2021-04-11 19:28:38 +0200

    uadded remote_require facility

--- www/public/medias/rubies/examples/code_editor.rb
+++ www/public/medias/rubies/examples/code_editor.rb
@@ -2,7 +2,7 @@
 
 t=text(content: "touch me to open the code editor", xx: 50)
 t.touch do
-  code({atome_id: :code_editor, content: :box})
+  code({atome_id: :code_editor, content: :circle})
 end
 
 code_editor_font_size=12
