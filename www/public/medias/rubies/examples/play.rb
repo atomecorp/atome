5b2a773b07788f1f01c33a36850398f9cc90bbca
c1ipro <lesylvs@mac.com>
2021-04-14 22:57:19 +0200

    work from Equinoxe

--- www/public/medias/rubies/examples/play.rb
+++ www/public/medias/rubies/examples/play.rb
@@ -1,6 +1,6 @@
 # play videos
 
-v=video({atome_id: :the_video, atome_id: :the_video_killer})
+v=video({atome_id: :the_video, atome_id: :the_video,shadow: {blur: 6 , thickness: 1, x: 3, y:3, color: :black}, drag: true})
 v.touch do
   v.play(true)
 end
---
+++ www/public/medias/rubies/examples/text_to_midi.rb
@@ -0,0 +1,22 @@
+# text to midi example
+
+# require "opal-parser"
+t=text("ok")
+
+cc = circle({width: 33, height: 33, y: 69, x: 69, color: :yellow, atome_id: :the_cc})
+
+ATOME.repeat 5, 0 do |evt|
+  t.content(evt.abs)
+  read("./medias/rubies/text_read.rb") do |datas_get|
+    datas=eval(datas_get)
+    temperature= datas[:temperature]
+    wind= datas[:wind]
+    humidity= datas[:humidity]
+    t.content( "temperature : #{temperature}, wind : #{wind}, humidity : #{humidity}")
+    # cc.transmit({midi: {play: {note: "C3", channel: 15, velocity: 10}}})
+    ATOME.wait 2 do
+      t.content(:stop)
+      # cc.transmit({midi: {stop: {note: "C3", channel: 15, velocity: 100}}})
+    end
+  end
+end
