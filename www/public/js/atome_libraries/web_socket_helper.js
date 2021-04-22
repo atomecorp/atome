81b445399b16ead182c5e2deb064a6908d4f018c
jean-ericgodard <jeezs@jeezs.net>
2021-04-22 10:08:06 +0200

    temp patch to catch websoket closing

--- www/public/js/atome_libraries/web_socket_helper.js
+++ www/public/js/atome_libraries/web_socket_helper.js
@@ -27,7 +27,6 @@ class WebSocketHelper {
     connect(serverAddress) {
         //fixme: find a way to restore the socket
         console.log("connection lost try ro reconnect");
-        // new WebSocketHelper(serverAddress);
     }
 
     sendMessage(type, message) {
