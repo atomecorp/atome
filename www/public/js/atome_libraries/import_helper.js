c730b55caefbc7f1492d17618e42e391dc375623
jean-ericgodard <jeezs@jeezs.net>
2021-05-13 10:42:10 +0200

    externalise js and css library

--- www/public/js/atome_libraries/import_helper.js
+++
@@ -1,8 +0,0 @@
-class ImportHelper {
-    addImage(parent, url) {
-        const randomId = Math.random().toString(16).substr(2, 32);
-        $('#' + parent).append('<img id="' + randomId + '"  width="500" height="600">');
-        const output = document.getElementById(randomId);
-        output.src = url;
-    }
-}
