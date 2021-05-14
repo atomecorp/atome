c9de06e722b1f2fa2b475e8a27b65deb8c90cd7f
jean-ericgodard <jeezs@jeezs.net>
2021-05-14 11:04:44 +0200

    enhanced alert method and authorisation example

--- atome/lib/atome/extensions/atome_utilities.rb
+++ atome/lib/atome/extensions/atome_utilities.rb
@@ -1,20 +1,20 @@
 def alert(message)
   if grab(:alert)
-    alert =grab(:alert)
-    alert_messages=grab(:alert_content)
-    new_content="#{alert_messages.content}#{message}\n"
+    alert_box = grab(:alert)
+    alert_messages = grab(:alert_content)
+    new_content = "#{alert_messages.content}#{message}\n"
     alert_messages.content(new_content)
     # alert.width=alert_messages.width
-    alert.height=alert.height+20
+    alert_box.height = alert_box.height + 20
   else
-    style=grab(:UI).content
-    alert=box({atome_id: :alert}.merge(style))
-    alert_messages= alert.text({atome_id: :alert_content, content: "#{message}\n"})
+    style = grab(:UI).content
+    alert_box = box(style.merge({ atome_id: :alert }))
+    alert_messages = alert_box.text({ x: 9, y: 3, atome_id: :alert_content, content: "#{message}\n" })
     # alert.width=alert_messages.width
-    alert.height=alert_messages.height
-    alert.touch do
+    alert_box.height = alert_messages.height
+    alert_box.touch do
       alert_messages.content("")
-      alert.delete(true)
+      alert_box.delete(true)
     end
   end
 end
--- www/public/medias/rubies/examples/authorisation.rb
+++ www/public/medias/rubies/examples/authorisation.rb
@@ -1,16 +1,21 @@
 # right management example
 
-b=box
+b = box({atome_id: :the_box, x: 99})
 b.touch do
-  b.authorization({ users: :all, write: false, password: :my_secret})
-  b.color(:red,"rong password")
-  b.color(:yellowgreen,b.authorization[:password])
-  b.x(99,:my_secret)
+  alert 'poil'
+  b.authorization({ users: :all, write: false, password: :my_secret })
+  b.color(:red, "false pass")
+  b.color(:yellowgreen, b.authorization[:password])
+  b.x(99, :my_secret)
+  b.width(99, :my_secret)
 end
 
-b2=b.box({x: 333})
+b2 = b.box({ x: 333, atome_id: :the_box2 })
 b2.drag(true)
-c=circle({y: 96})
+c = circle({ y: 96 ,atome_id: :the_circle})
 c.touch do
   clear(:view)
+  Atome.atomes.each do |atome|
+    alert atome[0]
+  end
 end
