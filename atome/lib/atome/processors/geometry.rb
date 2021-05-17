7b3acf8f3c5cd1470fc3fba1d834cb5422a4aa90
jean-ericgodard <jeezs@jeezs.net>
2021-05-17 08:22:45 +0200

    size api can now fit any object with x and y options

--- atome/lib/atome/processors/geometry.rb
+++ atome/lib/atome/processors/geometry.rb
@@ -1,6 +1,19 @@
 module Processors
   def size_pre_processor(value)
     if value.instance_of?(Hash) && value[:value].nil?
+      if value[:fit]
+        requested_width=grab(value[:fit]).width
+        requested_height=grab(value[:fit]).height
+        if value[:margin].instance_of?(Hash)
+          margin_x=value[:margin][:x]
+          margin_y=value[:margin][:y]
+        else
+          margin_x=value[:margin]
+          margin_y=value[:margin]
+        end
+        self.width=requested_width+margin_x
+        self.height=requested_height+margin_y
+      end
     else
       if value.instance_of?(Number) || value.instance_of?(Integer)
         value = { value: value }
@@ -30,6 +43,7 @@ module Processors
     if self.size
       value = self.size.merge(value)
     end
+
     @size = atomise(:size, value)
     size_html(value)
   end
