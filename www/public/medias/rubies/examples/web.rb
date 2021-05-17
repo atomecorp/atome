3c5f41be77ca69a832d8c371d575e44b2eaed050
jean-ericgodard <jeezs@jeezs.net>
2021-05-17 11:55:29 +0200

    add web_object example

---
+++ www/public/medias/rubies/examples/web.rb
@@ -0,0 +1,7 @@
+# web_object example
+
+my_tube = web('<iframe width="100%" height="180%" src="https://www.youtube.com/embed/usQDazZKWAk" frameborder="5" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+my_tube.x = 300
+my_tube.y = 150
+my_tube.drag(true)
+my_tube.width = 300
