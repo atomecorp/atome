082e6d0fd5c9e21483b4746b73beae296f0970f6
jean-ericgodard <jeezs@jeezs.net>
2021-05-18 10:39:09 +0200

    added intuition taxonomy

--- atome/lib/atome/environment/initialize.rb
+++ atome/lib/atome/environment/initialize.rb
@@ -6,7 +6,7 @@ ATOME = Atome.new({ atome_id: :atome, render: false, content: {} })
 Device.new
 MESSENGER = (grab(:messenger))
 # keyboard short cut
-grab(:device).key({option: :down}) do |evt|
+grab(:device).key({ option: :down }) do |evt|
   if evt.alt_key
     evt.stop_propagation
     evt.stop_immediate_propagation
@@ -14,7 +14,17 @@ grab(:device).key({option: :down}) do |evt|
     case evt.key_code
     when 65
       #key a
-      unless grab(:code_editor)
+      if grab("code_editor")
+        #the line below store the current state in the buffer object
+        grab(:buffer).content = grab(:buffer).content.merge(
+          code_editor: { content: ATOME.get_ide_content(:code_editor.to_s + "_code_editor"),
+                         x: grab(:code_editor).x,
+                         y: grab(:code_editor).y,
+                         width: grab(:code_editor).width,
+                         height: grab(:code_editor).height })
+        grab("code_editor").delete
+        evt.prevent_default
+      else
         #the restore the  state found in the buffer object
         if grab(:buffer).content[:code_editor]
           code_editor_content = grab(:buffer).content[:code_editor][:content]
@@ -26,14 +36,14 @@ grab(:device).key({option: :down}) do |evt|
           code_editor_content = :box
           x_position = 0
           y_position = 0
-          width_size=150
-          height_size=150
+          width_size = 150
+          height_size = 150
         end
-        code({atome_id: :code_editor, content: code_editor_content,
-              x: x_position,
-              y: y_position,
-              width: width_size,
-              height: height_size
+        code({ atome_id: :code_editor, content: code_editor_content,
+               x: x_position,
+               y: y_position,
+               width: width_size,
+               height: height_size
              })
         evt.prevent_default
       end
@@ -55,17 +65,6 @@ grab(:device).key({option: :down}) do |evt|
       #key y
     when 90
       #key z
-      if grab(:code_editor)
-        #the line below store the current state in the buffer object
-        grab(:buffer).content = grab(:buffer).content.merge(
-          code_editor: {content: ATOME.get_ide_content(:code_editor.to_s + "_code_editor"),
-                        x: grab(:code_editor).x,
-                        y: grab(:code_editor).y,
-                        width: grab(:code_editor).width,
-                        height: grab(:code_editor).height})
-        grab("code_editor").delete
-        evt.prevent_default
-      end
     else
     end
   elsif evt.ctrl_key
