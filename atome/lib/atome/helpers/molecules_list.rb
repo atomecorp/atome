7b981a7026ba4344a8d4b90678ea28ed0d3356bd
jeezs <jeezs@jeezs.net>
2021-06-05 08:59:37 +0200

    new molecule and atome list concept

--- atome/lib/atome/helpers/molecules_list.rb
+++ atome/lib/atome/helpers/molecules_list.rb
@@ -1,13 +1,20 @@
-def atome_list
-  [:authorization,:share,:value, :x, :y, :z, :thickness, :red, :green, :blue, :alpha, :up, :down, :long, :double , :stop, :remove ]
-end
+# def atome_list
+#   [:authorization,:share,:value, :x, :y, :z, :thickness, :red, :green, :blue, :alpha, :up, :down, :long, :double , :stop, :remove ]
+# end
 def molecules_list
-  { share: [:authorization, :share],
+  { share: [{ authorization: [:read,:write] }],
     blur: [:value],
-    shadow: [:blur, :x,:y, :thickness, :color] ,
+    shadow: [:blur, :x,:y, :thickness, { color:[:red, :green, :blue, :x, :y, diffusion: [:radial, :linear, :conic]] }] ,
     smooth: [:value],
     touch: [{ options: [:up, :down,:long, :double, :stop, :remove] }],
-    drag: [{ options: [:destroy, :disable, :containment, :lock, :handle] }],
-    options: [:up, :down,:long, :double, :stop, :remove, :destroy, :disable, :containment, :lock, :handle ]
+    drag: [{ options: [:destroy, :disable, :containment, :lock, :handle] }]
   }
+end
+
+
+molecules=[]
+atomes=[]
+molecules_list.each do |molecule, atome_or_molecule|
+  puts
+
 end
