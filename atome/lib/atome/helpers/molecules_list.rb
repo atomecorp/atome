eb341637bf5c6c02a2634e9c55e541c8351c04b1
jeezs <jeezs@jeezs.net>
2021-06-07 12:04:52 +0200

    automating tool creation

--- atome/lib/atome/helpers/molecules_list.rb
+++ atome/lib/atome/helpers/molecules_list.rb
@@ -1,20 +1,33 @@
-# def atome_list
-#   [:authorization,:share,:value, :x, :y, :z, :thickness, :red, :green, :blue, :alpha, :up, :down, :long, :double , :stop, :remove ]
-# end
-def molecules_list
-  { share: [{ authorization: [:read,:write] }],
-    blur: [:value],
-    shadow: [:blur, :x,:y, :thickness, { color:[:red, :green, :blue, :x, :y, diffusion: [:radial, :linear, :conic]] }] ,
-    smooth: [:value],
-    touch: [{ options: [:up, :down,:long, :double, :stop, :remove] }],
-    drag: [{ options: [:destroy, :disable, :containment, :lock, :handle] }]
-  }
+@molecules_list= { share: [{ authorization: [:read,:write] }],
+                  blur: [:value],
+                  shadow: [:blur, :x,:y, :thickness, { color:[:red, :green, :blue, :x, :y, diffusion: [:radial, :linear, :conic]] }] ,
+                  smooth: [:value],
+                  touch: [{ options: [:up, :down,:long, :double, :stop, :remove] }],
+                  drag: [{ options: [:destroy, :disable, :containment, :lock, :handle] }]
+}
+
+
+@tools=[]
+@atomes=[]
+def molecule_analysis(molecules)
+  molecules.each do |molecule, array_content|
+    @tools << molecule
+    array_content.each do |content|
+      if content.instance_of?(Symbol)
+        @atomes << content
+      else
+        molecule_analysis(content)
+      end
+    end
+  end
+  {molecules: molecules,tools: @tools, atomes: @atomes}
 end
 
 
-molecules=[]
-atomes=[]
-molecules_list.each do |molecule, atome_or_molecule|
-  puts
 
-end
+
+# notif @atomes
+
+
+# todo add atome and molecule list inside mol api it self instead of this standalone list
+# tod
