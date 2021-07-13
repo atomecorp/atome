27a2d6e018f0a389ae66db5c205bb80340ede48f
jeezs <jeezs@jeezs.net>
2021-07-13 21:32:58 +0200

    working on  multi rendering engine

--- www/public/js/atome_libraries/renderer_helper.js
+++ www/public/js/atome_libraries/renderer_helper.js
@@ -1,5 +1,6 @@
 // renderer global utils
-function dyn_lib_load(library, datas, id) {
+function dyn_lib_load(library, id, params) {
+
     let achieve = new Promise(function (resolve, reject) {
         var url = "js/third_parties/rendering_engines/" + library + ".min.js";
         $.getScript(url, function () {
@@ -10,84 +11,82 @@ function dyn_lib_load(library, datas, id) {
         // we have the renderer to renderer list
         Opal.Atome.$libraries(library);
         // now we run the  fabric renderer
-        eval(library + "('" + datas + "','" + id + "')");
+        switch (library) {
+            case 'fabric':
+                fabric(id, params);
+                break;
+            case 'three':
+                three(id, params);
+            default:
+            // console.log(`Sorry, we are out of ${expr}.`);
+        }
+
     });
 }
-// fabric renderer
-const  fabric_list={};
 
-function add_fabric_object(canvas, type, params){
+// fabric renderer
+const fabric_list = {};
+
+function add_fabric_object(canvas, params) {
     // first we test if the renderer is ready
-    if ($('#' +canvas).length===0){
+    if ($('#' + canvas).length === 0) {
         setTimeout(function () {
-            add_fabric_object(canvas, type, params);
+            add_fabric_object(canvas, params);
         }, 1);
-    }
-    else{
+    } else {
         // the renderer is ready ! here we go!
-        canvas=fabric_list[canvas];
+        canvas = fabric_list[canvas];
         var object;
-        params=params.$to_n();
-        // params=Opal.eval(params);
-        alert (params.radius);
-        switch (type) {
+        params = params.$to_n();
+        switch (params.type) {
             case 'circle':
-                 object = new fabric.Circle({
-                    // radius: 16, fill: 'red', left: 200, top: 100
+                object = new fabric.Circle({
+                    radius: params.radius, fill: params.fill, left: params.left, top: params.top
                 });
                 break;
             case 'rect':
-                 object = new fabric.Rect({
-                    id: 'myidj',
-                    left: 100,
-                    top: 100,
-                    fill: 'blue',
-                    width: 20,
-                    height: 20
+                object = new fabric.Rect({
+                    id: params.id,
+                    left: params.left,
+                    top: params.top,
+                    fill: params.fill,
+                    width: params.width,
+                    height: params.height
                 });
                 break;
             case 'image':
                 break;
             default:
-
-                // console.log(`Sorry, we are out of ${expr}.`);
+            // console.log(`Sorry, we are out of ${expr}.`);
         }
         canvas.add(object);
     }
 }
 
-function reportWindowSize() {
+function report_fabric_window_size() {
     // add to main atome's on_resize method
     canvas.setWidth(window.innerWidth);
     canvas.setHeight(window.innerHeight);
     canvas.calcOffset();
 }
 
-function fabric(datas, the_id) {
+function fabric(the_id, params) {
     var library = "fabric";
-
     if (Opal.Atome.$initialised_libraries(library)) {
-        /// create canvas
+        // create canvas
         var elemClientWidth = window.innerWidth;
         var elemClientHeight = window.innerHeight;
         $("#view").append("<canvas id='" + the_id + "' width=" + elemClientWidth + " height= " + elemClientHeight + "></canvas>");
-/// lib init
-        window.addEventListener('resize', reportWindowSize);
+        // lib init
+        window.addEventListener('resize', report_fabric_window_size);
         // create a wrapper around native canvas element (with id="c")
         var canvas = new fabric.Canvas(the_id);
         // we had the canvas to the canvas list
-        fabric_list[the_id]=canvas;
-
+        fabric_list[the_id] = canvas;
         ///object creation
-        add_fabric_object("canvas_id", 'rect',"radius: 20, fill: 'green', left: 100, top: 100");
-//         var circle = new fabric.Circle({
-//             radius: 20, fill: 'green', left: 100, top: 100
-//         });
-// // "add" rectangle onto canvas
-//         canvas.add(circle);
-//         // alert(fabric_list[the_id]);
-//         // alert(fabric_list["canvas_id"]);
+        add_fabric_object("canvas_id", params);
     } else {
-        dyn_lib_load(library, datas, the_id);
+
+        dyn_lib_load(library, the_id, params);
     }
 }
