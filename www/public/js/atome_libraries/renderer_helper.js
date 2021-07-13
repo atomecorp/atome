6ba4cf2ae6d090bb47bf3473a233a2b13b313212
jeezs <jeezs@jeezs.net>
2021-07-13 23:45:27 +0200

    continue to work on multi rendering engine

--- www/public/js/atome_libraries/renderer_helper.js
+++ www/public/js/atome_libraries/renderer_helper.js
@@ -9,7 +9,7 @@ function dyn_lib_load(library, id, params) {
     });
     achieve.then(function () {
         // we have the renderer to renderer list
-        Opal.Atome.$libraries(library);
+        // Opal.Atome.$libraries(library);
         // now we run the  fabric renderer
         switch (library) {
             case 'fabric':
@@ -17,6 +17,7 @@ function dyn_lib_load(library, id, params) {
                 break;
             case 'three':
                 three(id, params);
+                break;
             default:
             // console.log(`Sorry, we are out of ${expr}.`);
         }
@@ -31,8 +32,9 @@ function add_fabric_object(canvas, params) {
     // first we test if the renderer is ready
     if ($('#' + canvas).length === 0) {
         setTimeout(function () {
-            add_fabric_object(canvas, params);
+            add_fabric_object(canvas, params, false);
         }, 1);
+
     } else {
         // the renderer is ready ! here we go!
         canvas = fabric_list[canvas];
@@ -40,19 +42,10 @@ function add_fabric_object(canvas, params) {
         params = params.$to_n();
         switch (params.type) {
             case 'circle':
-                object = new fabric.Circle({
-                    radius: params.radius, fill: params.fill, left: params.left, top: params.top
-                });
+                object = new fabric.Circle(params);
                 break;
             case 'rect':
-                object = new fabric.Rect({
-                    id: params.id,
-                    left: params.left,
-                    top: params.top,
-                    fill: params.fill,
-                    width: params.width,
-                    height: params.height
-                });
+                object = new fabric.Rect(params);
                 break;
             case 'image':
                 break;
@@ -63,30 +56,36 @@ function add_fabric_object(canvas, params) {
     }
 }
 
-function report_fabric_window_size() {
-    // add to main atome's on_resize method
-    canvas.setWidth(window.innerWidth);
-    canvas.setHeight(window.innerHeight);
-    canvas.calcOffset();
-}
-
 function fabric(the_id, params) {
     var library = "fabric";
     if (Opal.Atome.$initialised_libraries(library)) {
         // create canvas
         var elemClientWidth = window.innerWidth;
         var elemClientHeight = window.innerHeight;
+
         $("#view").append("<canvas id='" + the_id + "' width=" + elemClientWidth + " height= " + elemClientHeight + "></canvas>");
         // lib init
-        window.addEventListener('resize', report_fabric_window_size);
         // create a wrapper around native canvas element (with id="c")
         var canvas = new fabric.Canvas(the_id);
         // we had the canvas to the canvas list
         fabric_list[the_id] = canvas;
+        $(window).resize(function () {
+            canvas.setWidth(window.innerWidth);
+            canvas.setHeight(window.innerHeight);
+            canvas.calcOffset();
+        });
+
         ///object creation
         add_fabric_object("canvas_id", params);
     } else {
-
+        // we have the renderer to renderer list
+        Opal.Atome.$libraries(library);
         dyn_lib_load(library, the_id, params);
     }
+}
+
+// three renderer
+
+function three(the_id, params) {
+
 }
