80ca0ef66077ba3f2168c22357e0fd60f07fdf96
jeezs <jeezs@jeezs.net>
2021-07-26 12:48:45 +0200

    Better map example

--- www/public/medias/rubies/examples/map.rb
+++ www/public/medias/rubies/examples/map.rb
@@ -1,6 +1,6 @@
 # navigation example
 
-b=box({width: 300, height: 333, atome_id: :navigator_1, shadow: true})
+b=box({width: 300, height: 300,x: 9,y: 9, atome_id: :navigator_1, shadow: true})
 b.map({ longitude: 55.9876876, lattitude: 33.987687 })
 
-circle(size: 300, x: 333, shadow: true, map: true)
+circle(size: 300, x: 366,y: 9, shadow: true, map: true)
