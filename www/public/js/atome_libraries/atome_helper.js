b9fdd753a3b24c2b0ee0d439b5ef19521b3c88af
jeezs <jeezs@jeezs.net>
2021-07-26 12:46:01 +0200

    map works with safari!!

--- www/public/js/atome_libraries/atome_helper.js
+++ www/public/js/atome_libraries/atome_helper.js
@@ -17,7 +17,6 @@ const atome = {
             }
             else{
                 navigator.geolocation.getCurrentPosition(function(position) {
-                    alert(position.coords.latitude);
                     var mymap = L.map(id).setView([position.coords.latitude, position.coords.longitude], 15);
                     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
                         maxZoom: 18,
