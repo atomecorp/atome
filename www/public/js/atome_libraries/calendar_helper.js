e484ad4436b094a1d58a636e780e19b322da7c5d
jeezs <jeezs@jeezs.net>
2021-07-27 10:01:34 +0200

    calendar concept + example added

---
+++ www/public/js/atome_libraries/calendar_helper.js
@@ -0,0 +1,88 @@
+function calendar(){
+    var COMMON_CUSTOM_THEME = {
+        'common.border': '3px solid #ffbb3b',
+        'common.backgroundColor': '#ffbb3b0f',
+        'common.holiday.color': '#f54f3d',
+        'common.saturday.color': '#3162ea',
+        'common.dayname.color': '#333'
+    };
+
+    var cal2, resizeThrottled;
+    var useCreationPopup = true;
+    var useDetailPopup = true;
+    var datePicker, selectedCalendar;
+
+    var cal = new tui.Calendar('#calendar', {
+        defaultView: 'month',
+        // defaultView: 'week',
+        taskView: true,    // Can be also ['milestone', 'task']
+        scheduleView: true,  // Can be also ['allday', 'time']
+        theme: COMMON_CUSTOM_THEME, // set theme
+        month: {
+            daynames: ['Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam', 'Dim'],
+            startDayOfWeek: 0,
+            // narrowWeekend: true
+        },
+        week: {
+            daynames: ['Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam', 'Dim'],
+            startDayOfWeek: 0,
+            // narrowWeekend: true
+        }
+    });
+    cal.on({
+        'clickMore': function(e) {
+            console.log('clickMore', e);
+        },
+        'clickSchedule': function(e) {
+            console.log('clickSchedule', e);
+        },
+        'clickDayname': function(date) {
+            console.log('clickDayname', date);
+        },
+        'beforeCreateSchedule': function(e) {
+            console.log('beforeCreateSchedule', e);
+            cal.changeView('week', true);
+
+            // saveNewSchedule(e);
+        },
+        'beforeUpdateSchedule': function(e) {
+            var schedule = e.schedule;
+            var changes = e.changes;
+
+            console.log('beforeUpdateSchedule', e);
+
+            if (changes && !changes.isAllDay && schedule.category === 'allday') {
+                changes.category = 'time';
+            }
+
+            cal.updateSchedule(schedule.id, schedule.calendarId, changes);
+            refreshScheduleVisibility();
+        },
+        'beforeDeleteSchedule': function(e) {
+            console.log('beforeDeleteSchedule', e);
+            cal.deleteSchedule(e.schedule.id, e.schedule.calendarId);
+        },
+        'afterRenderSchedule': function(e) {
+            var schedule = e.schedule;
+            // var element = cal.getElement(schedule.id, schedule.calendarId);
+            // console.log('afterRenderSchedule', element);
+        },
+        'clickTimezonesCollapseBtn': function(timezonesCollapsed) {
+            console.log('timezonesCollapsed', timezonesCollapsed);
+
+            if (timezonesCollapsed) {
+                cal.setTheme({
+                    'week.daygridLeft.width': '77px',
+                    'week.timegridLeft.width': '77px'
+                });
+            } else {
+                cal.setTheme({
+                    'week.daygridLeft.width': '60px',
+                    'week.timegridLeft.width': '60px'
+                });
+            }
+
+            return true;
+        }
+    });
+}
---
+++ www/public/medias/rubies/examples/calendar.rb
@@ -0,0 +1,4 @@
+# calendar example
+
+box({atome_id: :calendar, width: 666, height: 666})
+`calendar()`
