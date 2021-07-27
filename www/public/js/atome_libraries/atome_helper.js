7bb1a5c4cbf521a9c93514fdba5130b07f8c0ee2
jeezs <jeezs@jeezs.net>
2021-07-27 13:46:12 +0200

    debug map api when sending coordinate

--- www/public/js/atome_libraries/atome_helper.js
+++ www/public/js/atome_libraries/atome_helper.js
@@ -1,7 +1,7 @@
 const atome = {
     jsMap: function (id,longitude, lattitude,) {
         if ("geolocation" in navigator) {
-            if (typeof(longitude)==Number){
+            if (typeof(longitude)== "number"){
                 var mymap = L.map(id).setView([longitude, lattitude], 6);
                 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
                     maxZoom: 18,
