fd170fd3ab66315c60d6d8d8fe14cb5ed4a2bc9a
jeezs <jeezs@jeezs.net>
2021-07-30 13:46:59 +0200

    better meteo example

--- www/public/medias/rubies/examples/meteo.rb
+++ www/public/medias/rubies/examples/meteo.rb
@@ -1,35 +1,10 @@
 ########################### meteo ####################################
 
-
 def input_text(default_text)
   t = text({ content: default_text, x: 333, y: 3, atome_id: :my_text_input })
   t.visual(33)
-  # t.key(:down) do |evt|
-    # w_get = JSUtils.client_width(t.atome_id)
-    # t.width = :auto
-    # t.width = if w_get > 300
-    #             300
-    #           else
-    #             :auto
-    #           end
-  # end
 
   t.key(:up) do |evt|
-#     # todo: get and limit number of char
-#     `
-#      var myDiv = $('#my_tex_input');
-#     myDiv.text(myDiv.text().substring(0,15))
-# //alert(myDiv.text().length);
-# `
-#     w_get = JSUtils.client_width(t.atome_id)
-#     t.width = if w_get < 99
-#                 99
-#               elsif w_get > 300
-#                 300
-#               else
-#                 :auto
-#               end
-#     JSUtils.client_height(t.atome_id)
 
   end
 
@@ -47,19 +22,20 @@ end
 
 input_text("clermont-ferrand")
 
-
+text({ content: "temperature", y: 66, atome_id: :meteo_result })
 
 def meteo_of(town)
   meteo(town) do |data|
-    text({content: "la temperature a #{town} est de : "+ data.to_s, y:66})
+    grab(:meteo_result).content("la temperature a #{town} est de : " + data.to_s)
   end
 end
 
-
 meteo_of("clermont-ferrand")
 
-b=box({x:300, y:3, size:33, color: :red})
+b = box({ x: 300, y: 3, size: 33, color: :red })
 
 b.touch do
-  meteo_of grab(:my_text_input).content
+  content_found = grab(:my_text_input).content.read
+  content_toe_send = content_found[grab(:view).language] || content_found[:default]
+  meteo_of content_toe_send
 end
