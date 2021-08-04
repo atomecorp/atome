0beea1d6e2af1da49dc4cad3967a31432c905f03
jeezs <jeezs@jeezs.net>
2021-08-04 18:18:21 +0200

    in overflow demo the shape can be movde now

--- www/public/medias/rubies/examples/overflow.rb
+++ www/public/medias/rubies/examples/overflow.rb
@@ -2,7 +2,7 @@
 
 
 b = text({x: 369, y: 30, content: "touch me to change text overflow"})
-container=circle({width:300, height: 300, y: 99})
+container=circle({width:300, height: 300, y: 99, drag: true})
 container.text({content: lorem, x: 66, width: 300})
 i=0
 b.touch do
