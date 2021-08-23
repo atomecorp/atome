768f7dbf2536ce3e13f6018d98d942a18d139aa1
c1ipro <lesylvs@mac.com>
2021-08-23 18:45:20 +0200

    New better insert example

--- www/public/medias/rubies/examples/insert_attach.rb
+++ www/public/medias/rubies/examples/insert_attach.rb
@@ -1,5 +1,13 @@
 # insert example
 
-b=box({drag: true})
-c=circle({drag: true, size: 69, x: 33})
+b=box({ color: :lightgray})
+b.drag(true)
+bb=box({ size: 20, x: -10 })
+c=circle({  x: 35, color: :lightgray})
 b.insert(c.atome_id)
+b.insert(bb.atome_id)
+
+b.drag(true)
+b.shadow(true)
+b.x(300)
+b.y(300)
