1873b70b38b84fc954c6eb3a3fde4f8ba1cb074e
jean-ericgodard <jeezs@jeezs.net>
2021-09-06 19:15:52 +0200

    Back to demo

--- www/public/medias/rubies/examples/code.rb
+++ www/public/medias/rubies/examples/code.rb
@@ -1,6 +1,6 @@
 #code example
 
-text(content: "the code within ies executed, it'll create a green circle, touch the red circle ",code: "c=circle;c.color(:green)\nc.x(66)\nc.y(66)")
+text(content: "the code within the contain circle is executed, it'll create a green circle, \ntouch the red circle ",code: "c=circle;c.color(:green)\nc.x(66)\nc.y(66)")
 cell({ content: "box()", exec: false,atome_id: :the_box })
 cell({ content: "c=circle({x:300, y: 300});c.touch do \n grab(:the_box).exec(true)\nend" })
 
