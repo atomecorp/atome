d01ec6a5a7bb7ca975034756e6b3dc1d47fa10e8
jean-ericgodard <jeezs@jeezs.net>
2022-03-27 20:43:56 +0200

    The new Arc seems to be efficient simple and fast

--- Work&research/v2concept.rb
+++ Work&research/v2concept.rb
@@ -1,9 +1,16 @@
 class Universe
   def self.creator(user)
     @user = user
+    @atomes=[]
     Atome.new({ universe: :universe })
   end
+  def self.atomes_add(new_atome)
+    @atomes << new_atome
+  end
 
+  def self.atomes
+    @atomes
+  end
   def self.active_creator
     @user
   end
@@ -37,6 +44,7 @@ class Atome
     params.each do |property, value|
       @atome[property] = Particle.new(property, value)
     end
+    Universe.atomes_add(@atome)
     @atome
   end
 
@@ -44,13 +52,54 @@ class Atome
     @atome[:type]
   end
 
-  # def preset
+  def property_common(property, value, dynamic)
+    if value.nil?
+      @atome[property]
+    elsif value.instance_of?(Particle) && dynamic
+      @atome[property] = value
+    elsif value.instance_of?(Particle)
+      value.value.each do |value_found|
+        @atome[property] = Particle.new(property, value_found)
+      end
+    else
+      # send "set_#{property}_value", value
+      @atome[property] = Particle.new(property, value)
+    end
+  end
+
+  def atome_router(property, value, options, stack_property)
+    value = send("#{property}_pre_processor", value) if options[:pre_process]
+    # if the pre_processor decide that the value shouldn't be store and render it must return nil
+    atome_creation(property, value, stack_property) if options[:store_property] != false
+    send_to_render_engine(property, value) if options[:render_property] != false
+    value = send("#{property}_post_processor", value) if options[:post_process]
+    method_return value
+    @atome[property]
+  end
+
+  ########## solution
+
+  # def set_preset_value(value)
+  #   @atome[:preset] = Particle.new(:preset, value)
+  # end
+  #
+  # def set_preset_particle(value)
+  #   @atome[:preset] = value
+  # end
+  #
+  # def get_preset
   #   @atome[:preset]
   # end
 
-  def preset=(value)
-    @atome[:preset] = Particle.new(:preset, value)
+  def preset(value = nil, dynamic = false)
+    property_common(:preset, value, dynamic)
   end
+
+  def preset=(value)
+    preset(value)
+  end
+
+  ##########
 
   def add(params)
     params.each do |property, value|
@@ -58,6 +107,7 @@ class Atome
     end
   end
 
+  ###### solution 2
   def put(property, value)
     @atome[property] = Particle.new(property, value)
   end
@@ -66,6 +116,7 @@ class Atome
     @atome[property].value
   end
 
+  #######
   def adds(params)
     params.each do |param|
       param.each do |property, value|
@@ -105,9 +156,14 @@ a = Atome.new({ type: :shape, preset: :box, id: :the_box,
 # puts a.type.inspect
 
 a.add({ preset: :circle })
-a.adds([{ preset: :circle }])
-puts a.read(:preset)
-a.preset = :toto
-a.put(:preset,:cube)
-puts a.read(:preset)
+a.adds([{ preset: :triangle }, { preset: :image }])
+# puts a.read(:preset)
+a.preset(:toto)
+# a.put(:preset, :cube)
+# puts a.read(:preset)
 # puts a.preset.class
+# puts a.preset.value
+a.preset(a.type) #dynamic
+# a.preset(a.type, :dynamic) # static
+puts a.preset.inspect
+puts Universe.atomes.length
