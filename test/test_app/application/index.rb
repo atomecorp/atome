0b60a4f1f056f84f7679fc488b6e38b1e1d6a52a
Jean-Eric Godard <jeezs@jeezs.net>
2022-10-15 16:06:26 +0200

    Drag callback added

--- test/test_app/application/index.rb
+++ test/test_app/application/index.rb
@@ -68,12 +68,65 @@ b.top(99)
 # end
 
 
-
 b.rotate(999) do
   alert :poilu
 end
 
+def dragCallback(page_x,page_y)
+  puts "#{page_x}:#{page_y}"
+end
+
 # drag example
+
+`// target elements with the "draggable" class
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ],
+    // enable autoScroll
+    autoScroll: true,
+
+    listeners: {
+      // call this function on every dragmove event
+      move: dragMoveListener,
+
+      // call this function on every dragend event
+      end (event) {
+        var textEl = event.target.querySelector('p')
+
+        textEl && (textEl.textContent =
+          'moved a distance of ' +
+          (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                     Math.pow(event.pageY - event.y0, 2) | 0))
+            .toFixed(2) + 'px')
+      }
+    }
+  })
+
+function dragMoveListener (event) {
+
+  var target = event.target
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  // translate the element
+  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+// CallBack here
+  Opal.Atome.$dragCallback(event.pageX,event.pageY);
+}
+`
 Genesis.particle_creator(:drag)
 Genesis.generate_headless_renderer(:drag) do |value, atome, user_proc|
   puts "msg from headless drag method: value is #{value} , atome class is #{atome.class}"
@@ -81,7 +134,11 @@ Genesis.generate_headless_renderer(:drag) do |value, atome, user_proc|
 end
 
 Genesis.generate_html_renderer(:drag) do |value, atome, user_proc|
-  puts "msg from html drag method: value is #{value} , atome class is #{atome.class}"
+  if value == :remove
+    @html_object.remove_class(:draggable)
+  else
+    @html_object.add_class(:draggable)
+  end
   instance_exec(&user_proc) if user_proc.is_a?(Proc)
 end
 # it work :
@@ -99,6 +156,12 @@ a = Atome.new(
 # puts a.inspect
 a.shape.drag(true) do
   puts :drag_is_ok
+end
+
+a.shape.touch(true) do
+   # @html_object.remove_class(:draggable)
+   a.shape.drag(:remove)
+  # @html_object.remove_class(:draggable)
 end
 # Genesis.atome_creator_option(:color_pre_save_proc) do |params|
 #   # puts "1- optional color_pre_save_proc: #{params[:params].class}\n"
