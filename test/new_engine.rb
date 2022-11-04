0299552d496829031b345907392869574a3d7808
jeezs <jeezs@jeezs.net>
2022-11-04 13:50:15 +0100

    new architecture testing

--- test/new_engine.rb
+++ test/new_engine.rb
@@ -1,7 +1,7 @@
 class Atome
 
   def shape(params)
-    @atome=params
+    @atome = params
   end
 
   def initialize(params = {}, &bloc)
@@ -11,7 +11,7 @@ class Atome
     # # TODO: check if we need to add properties for the root object before sending the params
     # puts params[:type]
     # puts "------"
-    send(params[:type],params )
+    send(params[:type], params)
   end
 
   def left
@@ -22,38 +22,45 @@ class Atome
     @atome[:right]
   end
 
-  def particle val
-    puts "the val is #{val}"
+  def particle
+    @particle
   end
 
+  def set_particle val
+    # puts "the val is #{val}"
+    @particle = val
+  end
 
   def color
-    @particle= Atome.new({type: :particle,particle: :color, value: @atome[:color]} )
+    Atome.new({ type: :set_particle, particle: :color, value: @atome[:color] })
   end
 
   def value
-    @particle
+    ({id:  @particle[:value].keys[0] } ).merge(@particle[:value].values[0][:value])
+
   end
 
   def values
-    @particle
+    values={}
+    @particle[:value].each do |k,v|
+      values[k]=v[:value]
+      # puts k
+      # puts ({ id: k }).merge(v[:value])
+      #  puts "----"
+    end
+    values
   end
-
-  def to_s
-    # values
-  end
-
 
 end
 
 # a=Atome.new({type: :shape,left: [{ id: :a_0, value: 33 } ], color: [{id: :a_1, value: :red }, {id: :a_1, value: :green }]})
 
-a=Atome.new({type: :shape,left: { a_0: 33 } , color:{a1: :red, a2: :green}})
+a = Atome.new({ type: :shape, left: { a_0: {value:  33 } }, color: { a1: { value: { red: 1, green: 0 } }, a2: {value: { red: 0.3, green: 0 }} } })
 
 # puts "a.color : #{a.color}"
 # puts "a.color.class : #{a.color.class}"
-# puts "a.color.values : #{a.color.values}"
+puts "a.color.values : #{a.color.values}"
 puts "a.color.value : #{a.color.value}"
- # a.color.each do |k,v|
- #   puts "v is :#{v}"
- # end
+# a.color.each do |k,v|
+#   puts "v is :#{v}"
+# end
