c2a3ed7cbc1668b6ed70b219a0c38c087b5fea37
Jean-Eric Godard <jeezs@jeezs.net>
2022-12-05 09:23:52 +0100

    Working on a matrix

--- test/test_app/application/index.rb
+++ test/test_app/application/index.rb
@@ -28,6 +28,7 @@
 # # TODO : keep complex property when animating (cf shadows)
 # # TODO : at method is bugged : my_video.at accumulate at each video play
 # # TODO : check the possibility of creation an instance variable for any particle proc eg : a.left do ... => @left_code
+# # TODO :  box().left(33).color(:red).smooth(8) doesn't work as atome try to smooth the color instead of the box
 
-require 'build/medias/rubies/examples/cursor'
+require 'build/medias/rubies/examples/_matrix'
 
