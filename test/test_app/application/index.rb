7e4bda45d3fa2e3807c552e1bc9f43cb203d7cb5
Jean-Eric Godard <jeezs@jeezs.net>
2022-12-07 22:34:14 +0100

    updated TODO list

--- test/test_app/application/index.rb
+++ test/test_app/application/index.rb
@@ -9,30 +9,30 @@
 # # TODO : look why get_particle(:children) return an atome not the value
 # # Done : create color JS for Opal?
 # # TODO : box callback doesnt work
-# # TODO : Application is minimized all the time, we must try to condition it
-# # TODO : A anew atome is created each time Genesis.generator is call, we better always use the same atome
-# # TODO : Decide with Regis if we create a new css, an inline style on object or alter the css as describe above
+# # TODO : User application is minimized all the time, we must try to condition it
+# # TODO : A new atome is created each time Genesis.generator is call, we better always use the same atome
+# # Done : Decide with Regis if we create a new css, an inline style on object or alter the css as describe above
 # # DOME: when applying atome.atome ( box.color), color should be added to the list of box's children
 # # DONE : server crash, it try to use opal_browser_method
 # # TODO : Check server mode there's a problem with color
 # # TODO : the function "jsReader" in atome.js cause an error in uglifier when using production mode
-# # TODO : add edit method
+# # Done : add edit method
 # # TODO : add add method to add children /parents/colors
 # # TODO : when drag update the atome's position of all children
 # # TODO : analysis on Bidirectional code and drag callback
 # # TODO : create shadow presets
 # # TODO : analysis on presets sanitizer confusion
-# # TODO : optimize the use of 'generator = Genesis.generator'
+# # TODO : optimize the use of 'generator = Genesis.generator' it should be at one place only
 # # TODO : Create a demo test of all apis
 # # TODO : animate from actual position to another given position
 # # TODO : keep complex property when animating (cf shadows)
-# # TODO : at method is bugged : my_video.at accumulate at each video play
-# # TODO : at method only accept one instance
+# # TODO : 'at' method is bugged : my_video.at accumulate at each video play
+# # TODO : 'at' method only accept one instance
 # # Done : check the possibility of creation an instance variable for any particle proc eg : a.left do ... => @left_code
-# # TODO : color, shadow, ... must be add as components not children
-# # TODO :  box().left(33).color(:red).smooth(8) doesn't work as atome try to smooth the color instead of the box
+# # Done : color, shadow, ... must be add as 'attach' not children
 # # TODO :  box().left(33).color(:red).smooth(8) doesn't work as atome try to smooth the color instead of the box
 # # TODO : atome have both 'set_particle' and 'particle' instance variable, eg 'set_color' and 'color' (make a choice)
+
 # require 'build/medias/rubies/examples/color'
 # require 'build/medias/rubies/examples/animation'
 # wait 10 do
