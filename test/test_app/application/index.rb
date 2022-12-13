b08b1cb83a49007988da4ba478ef36871e8cf8f2
Jean-Eric Godard <jeezs@jeezs.net>
2022-12-13 19:15:16 +0100

    Change tauri json to be more permissive

--- test/test_app/application/index.rb
+++ test/test_app/application/index.rb
@@ -47,8 +47,8 @@
 
 # require 'src/medias/rubies/examples/_dataset'
 # require 'src/medias/rubies/examples/attach'
-require 'src/medias/rubies/examples/parents'
-# require 'src/medias/rubies/examples/markers'
+# require 'src/medias/rubies/examples/parents'
+require 'src/medias/rubies/examples/markers'
 # require 'src/medias/rubies/examples/add'
 # require 'src/medias/rubies/examples/matrix'
 # require 'src/medias/rubies/examples/color'
