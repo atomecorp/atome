5075a8d8e6967f494932f8b224f4224e3d929e6a
Jean-Eric Godard <jeezs@jeezs.net>
2023-02-14 17:28:17 +0100

    debug id generation, now all atomes use the identity_generator

--- test/test_app/application/index.rb
+++ test/test_app/application/index.rb
@@ -674,7 +674,7 @@
 # require 'src/medias/rubies/examples/bottom'
 # require 'src/medias/rubies/examples/attached'
 # require 'src/medias/rubies/examples/parents'
-# require 'src/medias/rubies/examples/markers'
+require 'src/medias/rubies/examples/markers'
 # require 'src/medias/rubies/examples/add'
 # require 'src/medias/rubies/examples/matrix'
 # require 'src/medias/rubies/examples/color'
@@ -738,61 +738,3 @@
 
 #################
 # frozen_string_literal: true
-
-separator = 120
-b = box({ left: separator, id: :test_box })
-c = box({ left: b.left + separator })
-d = box({ left: c.left + separator })
-e = box({ left: d.left + separator })
-
-b.touch(:down) do
-  b.color(:red)
-  c.color(:red)
-  d.color(:red)
-  e.color(:red)
-end
-b.text({ data: :down })
-
-c.touch(:long) do
-  # wait 1 do
-    b.color(:blue)
-    c.color(:blue)
-    d.color(:blue)
-    e.color(:blue)
-  # end
-
-end
-c.text({ data: :long })
-
-d.touch(:up) do
-  b.color(:yellow)
-  c.color(:yellow)
-  d.color(:yellow)
-  e.color(:yellow)
-end
-d.text({ data: :up })
-e.touch(:double) do
-  b.color(:black)
-  c.color(:black)
-  d.color(:black)
-  e.color(:black)
-end
-e.text({ data: :double })
-
-ccc = circle
-ccc.top(199)
-
-# def unbind(val=nil)
-#   id_found=self.atome[:id]
-# `
-# const el = document.getElementById(#{id_found});
-# interact('#'+#{id_found}).unset(#{val});
-# `
-# end
-b.touch(:long) do
-  b.color(:cyan)
-end
-
-ccc.touch(true) do
-  b.unbind(:tap)
-end
