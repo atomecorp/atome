369548e01ce1c444522b503cec8adda4beccf185
Jean-Eric Godard <jeezs@jeezs.net>
2023-10-15 12:19:10 +0200

    Added shell access to native apps

--- vendor/assets/src/js/atome/atome.js
+++ vendor/assets/src/js/atome/atome.js
@@ -1,1 +1,74 @@
 const atomeJS = Object.assign(communication, File);
+console.log('in tauri  mode we should add a js script to specify if tauri is active or not ')
+
+//call greet test
+// async function callGreet() {
+//     try {
+//         const response = await window.__TAURI__.invoke('greet', { name: 'John' });
+//         console.log(response); // Cela affichera la réponse dans la console
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+//
+// callGreet();
+
+//Send shell command
+async function callExecuteCommand(comd) {
+    try {
+        const command = comd; // Remplacez ceci par votre commande shell souhaitée
+        const response = await window.__TAURI__.invoke('execute_command', {command});
+        console.log(response); // Cela affichera la sortie de la commande ou l'erreur dans la console
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// we check if we are in tauri context
+if (typeof window.__TAURI__ === 'undefined') {
+
+} else {
+
+    callExecuteCommand('pwd');
+}
+
+
+// 'ls -l'
+
+
+//read/write
+//
+// // Créer un fichier
+// async function createAndWriteFile(filePath, contentToWrite) {
+//     try {
+//         await window.__TAURI__.invoke('write_file', {
+//             file_path: filePath,
+//             content: contentToWrite
+//         });
+//         console.log('File written successfully.');
+//     } catch (error) {
+//         console.error('Error writing to file:', error);
+//     }
+// }
+//
+// // Lire un fichier
+// async function readAndDisplayFile(filePath) {
+//     try {
+//         const fileContent = await window.__TAURI__.invoke('read_file', {
+//             file_path: filePath
+//         });
+//         console.log('File content:', fileContent);
+//     } catch (error) {
+//         console.error('Error reading file:', error);
+//     }
+// }
+//
+// // Utiliser les fonctions pour créer et lire le fichier
+// const filePath = 'example.txt'; // Remplacez ceci par le chemin du fichier souhaité
+// const contentToWrite = 'This is some content to write to the file.';
+//
+// // Créer un fichier avec le contenu
+// createAndWriteFile(filePath, contentToWrite);
+//
+// // Lire le fichier
+// readAndDisplayFile(filePath);
