cf04af13b5f7ee30641663a959b9a425b87ef37e
Jean-Eric Godard <jeezs@jeezs.net>
2023-11-15 14:58:12 +0100

    A few enhancement to video particle

--- vendor/assets/application/examples/play.rb
+++ vendor/assets/application/examples/play.rb
@@ -19,9 +19,16 @@ v.touch(true) do
 end
 
 t=text({id: :my_text, data: :hello})
+
 t.touch(true) do
+  v.data=0
   v.play(26) do |event|
     t.data("event is : #{event}")
+    if event[:frame] ==  900 && v.data <3
+      puts v.data
+      v.data(v.data+1)
+      v.play(26)
+    end
   end
 end
 
@@ -36,18 +43,15 @@ cc=circle({left: 0, width: 55, height: 55})
 left=0
 cc.drag(:locked) do |event|
   dx = event[:dx]
-  dy = event[:dy]
   left += dx.to_f
   min_left = 0
   max_left = 600
-
   left = [min_left, left].max
   left = [left, max_left].min
-
   v.html.currentTime(left/10)
   cc.left(left)
 end
 
 
 puts "add lock x and y when drag"
-puts "restrict view doesnt work"
+puts "restrict ro :view doesnt work"
