81678a111297b508c2f669171c8c64e3d0dfe8fb
Jean-Eric Godard <jeezs@jeezs.net>
2023-12-07 09:45:13 +0100

    layout particle almost finished

--- lib/atome/atome.rb
+++ lib/atome/atome.rb
@@ -17,6 +17,7 @@ class Atome
     @type = new_atome[:type] || :element
     @attached = []
     @category = []
+    @display = { }
     # @copy = []
     @html = HTML.new(@id, self)
     @headless = Headless.new(@id, self)
--- vendor/assets/application/examples/layout.rb
+++ vendor/assets/application/examples/layout.rb
@@ -29,7 +29,7 @@ class HTML
     @layout_style = { element: element_style, container: container_style }
   end
 
-  def remove_layout(element, container)
+  def remove_webview_layout(element, container)
     element[:classList].add('atome')
     element[:classList].remove('matrix_element')
   end
@@ -41,74 +41,99 @@ class HTML
     height_found = params[:height]
     layout_container = JS.global[:document].getElementById(id_found.to_s)
     layout_elements = JS.global[:document].querySelectorAll(".#{id_found}")
-    layout_style({ element: { backgroundColor: :purple } })
-
-    layout_container[:classList].remove('atome')
-    layout_container[:classList].add('matrix')
-    layout_elements.to_a.each do |element|
-      # element[:style][property] = value.to_s
-      element[:classList].remove('atome')
-      element[:classList].add('matrix_element')
-
-    end
-
-    # test remove layout
-    wait 2 do
+    if mode_found == :default
       layout_elements.to_a.each do |element|
-        remove_layout(element, layout_container)
+        element[:classList].remove('matrix_element')
+        element[:classList].add('atome')
+      end
+    else
+      # layout_style({ element: { backgroundColor: :purple } })
+      layout_container[:classList].remove('atome')
+      layout_container[:classList].add('matrix')
+      layout_elements.to_a.each do |element|
+        # element[:style][property] = value.to_s
+        element[:classList].remove('atome')
+        element[:classList].add('matrix_element')
       end
     end
   end
+end
 
+class Atome
+  def remove_layout
+    display[:mode] = :default
+    # we get the current parent (the previous layout)
+    parent_found = grab(attach)
+    # we get the parent of the parent
+    grand_parent = parent_found.attach
+    # and attach the item to the grand parent
+    attach(grand_parent)
+    #  we delete the parent (the layout) if it no more children attached
+    if parent_found.attached.length == 0
+      parent_found.delete(true)
+    end
+  end
 end
 
 new({ particle: :layout }) do |params|
-
   mode_found = params[:mode] ||= :list
   layout_width = params[:width] ||= 333
   layout_height = params[:height] ||= 333
-
   # now we get the list of the atome to layout
-  atomes_to_layout=[]
+  atomes_to_layout = []
   if type == :group
     atomes_to_layout = collect
   end
   # if params[:list] is specified group collection is override
   atomes_to_layout = params[:list] if params[:list]
 
-  if params[:id]
-    container_name = params[:id]
-    container = grab(:view).box({ id: container_name })
-    container_class = container_name
+  if mode_found == :default
+    # the user want to revert the layout to the default
+    atomes_to_layout.each do |atome_id_to_layout|
+      atome_to_layout = grab(atome_id_to_layout)
+      unless params[:id]
+        params[:id] = atome_to_layout.display[:layout]
+      end
+      atome_to_layout.remove_layout
+    end
+
   else
-    container = grab(:view).box
-    container_class = container.id
-  end
-  container.color({ alpha: 0.4 })
-  container.overflow(:scroll)
-  container.width = layout_width
-  container.height = layout_height
-  atomes_to_layout.each do |atome_id_to_layout|
-    atome_to_layout = grab(atome_id_to_layout)
-    atome_to_layout.attach(container_class)
-    atome_to_layout.html.add_class(container_class)
-  end
+    if params[:id]
+      container_name = params[:id]
+      container = grab(:view).box({ id: container_name })
+      container_class = container_name
+    else
+      container = grab(:view).box
+      id_found = container.id
+      params[:id] = id_found
+      container_class = id_found
+    end
 
-  # we return the list of atomes to layout
+    container.width = layout_width
+    container.height = layout_height
+    atomes_to_layout.each do |atome_id_to_layout|
+      atome_to_layout = grab(atome_id_to_layout)
+      # we remove previous display mode
+      atome_to_layout.remove_layout
+      atome_to_layout.display[:mode] = mode_found
+      atome_to_layout.display[:layout] = id_found
+      atome_to_layout.attach(container_class)
+      atome_to_layout.html.add_class(container_class)
+    end
+  end
   params
-
 end
 
 new({ renderer: :html, method: :layout, type: :hash }) do |params, _user_proc|
   html.layout(params)
 end
 
+new({ particle: :display })
+
 # system
 Atome.new({ renderers: [:html], id: :selector, collect: [], type: :group, tag: { system: true } })
 
-###############
-
-# we create
+############### tests
 
 b = box({ color: :red, id: :the_box, left: 3 })
 16.times do |index|
@@ -120,7 +145,7 @@ grab(:view).attached.each do |atome_found|
   grab(atome_found).selected(true)
 end
 grab(:the_box_copy_1).text(:hello)
-#################@
+#################
 selected_items = grab(Universe.current_user).selection # we create a group
 # we collect all atomes in the view
 atomes_found = []
@@ -141,11 +166,21 @@ random_found.each do |atome_id|
   end
 end
 wait 1 do
-  # alert selected_items.id
-  selected_items.layout({ id: :my_layout, mode: :grid, width: 500, height: 500 })
+  a = selected_items.layout({ mode: :grid, width: 500, height: 500 })
   # selected_items.layout({ id: :my_layout, mode: :grid, width: 500, height: 500 })
 
-  # wait 1 do
-  #   selected_items.layout({ id: :my_layout, mode: :grid, width: 500, height: 500 })
-  # end
+  wait 1 do
+    a = selected_items.layout({ mode: :grid, width: 800, height: 500 })
+
+    wait 1 do
+      # selected_items.layout({ id: :my_layout, mode: :list, width: 500, height: 500 })
+      selected_items.layout({ mode: :default, width: 500, height: 500 })
+      # selected_items.layout({  mode: :default, width: 500, height: 500 })
+      wait 1 do
+        selected_items.layout({ id: :my_layout, mode: :list, width: 500, height: 500 })
+      end
+    end
+  end
 end
+
+JS.global[:console].clear()
