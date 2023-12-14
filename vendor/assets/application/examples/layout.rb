7f199d6ee79f2f040e53640cab0301568bda9562
Jean-Eric Godard <jeezs@jeezs.net>
2023-12-14 23:13:01 +0100

    Layout now have list too..

--- vendor/assets/application/examples/layout.rb
+++ vendor/assets/application/examples/layout.rb
@@ -1,78 +1,5 @@
 # frozen_string_literal: true
 
-# class HTML
-#
-#   def layout_style(option = {})
-#     element_style = {
-#       backgroundColor: 'rebeccapurple',
-#       width: '100%',remove_layout
-#       height: '29px',
-#       borderRadius: '59px',
-#       display: 'flex',
-#       justifyContent: 'center',
-#       alignItems: 'center',
-#       flexDirection: 'column',
-#       marginBottom: '20px',
-#       padding: '0px',
-#       # transform: 'rotate(22deg)',
-#
-#     }
-#
-#     container_style = {
-#       display: :grid,
-#       gridTemplateColumns: 'repeat(4, 1fr)',
-#       overflow: :scroll,
-#       flexDirection: 'column',
-#       gap: '20px',
-#       padding: '0px',
-#
-#     }
-#     element_style = element_style.merge(option[:element]) if option[:element]
-#     container_style = container_style.merge(option[:container]) if option[:container]
-#     @layout_style = { element: element_style, container: container_style }
-#   end
-#
-#   def remove_webview_layout(element, container)
-#     element[:classList].add('atome')
-#     element[:classList].remove('matrix_element')
-#   end
-#
-#   def layout(params)
-#
-#     id_found = params[:id]
-#     mode_found = params[:mode]
-#     width_found = params[:width]
-#     height_found = params[:height]
-#     layout_container = JS.global[:document].getElementById(id_found.to_s)
-#     layout_elements = JS.global[:document].querySelectorAll(".#{id_found}")
-#     if mode_found == :default
-#       # we revert all elements to default state / layout
-#       layout_elements.to_a.each do |element|
-#         element[:classList].remove('matrix_element')
-#         element[:classList].add('atome')
-#       end
-#     else
-#       # we apply new layout to  elements
-#       layout_style({ element: { backgroundColor: :purple } })
-#       layout_container[:classList].remove('atome')
-#       layout_container[:classList].add('matrix')
-#       layout_elements.to_a.each do |element|
-#         # we check if current state is default if yes we store the layout to be able to restore it
-#         element[:style][:borderRadius] = '59px'
-#         element[:classList].remove('atome')
-#         # element[:classList].add('matrix_element')
-#       end
-#     end
-#
-#   end
-# end
-
-# class HTML
-#
-#   def
-#
-# end
-
 class Atome
   def remove_layout
     display[:mode] = :default
@@ -92,19 +19,21 @@ class Atome
   end
 end
 
+new({ particle: :organise })
+
+new({ method: :organise, type: :integer, renderer: :html }) do |params|
+  html.style(:gridTemplateColumns, params)
+end
 new({ particle: :layout }) do |params|
   mode_found = params.delete(:mode) || :list
   elements_style = params.delete(:element) || {}
-  # layout_width = params[:width] ||= 333
-  # layout_height = params[:height] ||= 333
   # now we get the list of the atome to layout
   atomes_to_layout = []
   if type == :group
     atomes_to_layout = collect
   end
-  # if params[:list] is specified group collection is override
-  atomes_to_layout = params[:list] if params[:list]
-
+  # if params[:listing] is specified group collection is override
+  atomes_to_layout = params[:listing] if params[:listing]
   if mode_found == :default
     # the user want to revert the layout to the default
     atomes_to_layout.each do |atome_id_to_layout|
@@ -112,12 +41,13 @@ new({ particle: :layout }) do |params|
       unless params[:id]
         params[:id] = atome_to_layout.display[:layout]
       end
+      # p "restoring => #{ atome_to_layout.display[:default]}"
       # now we get the default particles and restore it
       atome_to_layout.display[:default].each do |particle, value|
+        "-> now restoring, particle: #{particle}, value : #{value}"
         atome_to_layout.send(:delete, particle)
         atome_to_layout.send(particle, value)
       end
-
       atome_to_layout.remove_layout
     end
   else
@@ -133,12 +63,16 @@ new({ particle: :layout }) do |params|
     end
     container.remove({ category: :atome })
     container.category(:matrix)
-    puts "mode_found >> #{mode_found}"
+    # puts "mode_found >> #{mode_found}"
+    if mode_found == :list
+      params[:organise] = '1fr'
+
+    end
     params.each do |particle, value|
       container.send(particle, value)
     end
     # now we add user wanted particles
-    puts "params #{params}"
+    # puts "params #{params}"
 
     atomes_to_layout.each do |atome_id_to_layout|
       atome_to_layout = grab(atome_id_to_layout)
@@ -151,13 +85,33 @@ new({ particle: :layout }) do |params|
       atome_to_layout.category(container_class)
       # the hash below is used to restore element state
       original_state = {}
+      # alert atome_to_layout.display
+      # we only store the state if  atome_to_layout.display[:default]== {} it means this is the original state
       elements_style.each do |particle, value|
         # we have to store all elements particle to restore it later
-        original_state[particle] = atome_to_layout.send(particle)
+        # if atome_to_layout.display[:default] == {}
+        # puts "before #{atome_to_layout.display[:default]}"
+        unless atome_to_layout.display[:default].has_key?(particle)
+          # puts "==> #{atome_to_layout.display[:default][particle]} #{particle}<<= if theres something don't store"
+          # else
+          particle_to_save = atome_to_layout.send(particle) || 0
+          # puts "====> #{particle_to_save}"
+          original_state[particle] = particle_to_save
+        end
+        # puts "after #{atome_to_layout.display[:default]}"
+
+        # end
         atome_to_layout.send(particle, value)
       end
-      atome_to_layout.display[:default] = original_state
-      # atome_to_layout.html.add_class(container_class)
+      # we store the original state only if it is not empty
+      # puts original_state
+       unless original_state == {}
+         # atome_to_layout.display[:default].merge(original_state)
+         # puts "storing : #{original_state} =  #{atome_to_layout.display[:default]}"
+         atome_to_layout.display[:default]=atome_to_layout.display[:default].merge(original_state)
+         # atome_to_layout.html.add_class(container_class)
+       end
+
     end
   end
   params
@@ -188,29 +142,29 @@ selected_items.each do |atome_found|
   atomes_found << atome_found
 end
 
-# Simple verif
+# Simple check
 
-selected_items.layout({ id: :my_layout, mode: :list, width: 500, height: 800, overflow: :scroll })
+# selected_items.layout({ id: :my_layout, mode: :list, width: 500, height: 800, overflow: :scroll })
 
-# # full verif
-# wait 1 do
-#   selected_items.layout({ mode: :grid, width: 900, height: 500, color: :green,element: {height: 22, rotate: 22} })
-#   # alert grab('the_box_copy_4').display
-#   wait 1 do
-#     # selected_items.layout({ mode: :grid, width: 800, height: 500 })
-#     wait 1 do
-#       selected_items.layout({ mode: :default, width: 500, height: 500 })
-#       # alert grab('the_box_copy_4').display
-#       wait 1 do
-#         selected_items.layout({ id: :my_layout, mode: :list, width: 151, height: 800, overflow: :scroll })
-#         wait 1 do
-#           selected_items.layout({ mode: :default, width: 500, height: 500 })
-#         end
-#       end
-#     end
-#   end
-# end
-#
+# full check
+wait 1 do
+  selected_items.layout({ mode: :grid, width: 900, height: 500, color: :green, element: { rotate: 22, height: 21 } })
+
+  selected_items.layout({ mode: :grid, width: 900, height: 500, color: :green, element: { rotate: 22 } })
+  wait 1 do
+    selected_items.layout({ mode: :grid, width: 80, height: 500 , overflow: :scroll})
+    wait 1 do
+      selected_items.layout({ mode: :default, width: 500,  height: 22 })
+      wait 1 do
+        selected_items.layout({ id: :my_layout, mode: :grid, width: 200, height: 800, overflow: :scroll, element: { height: 22, width: 800 } })
+        wait 1 do
+          selected_items.layout({ mode: :default, width: 500, height: 500 })
+        end
+      end
+    end
+  end
+end
+
 #
 # # random test
 # random_found = atomes_found.sample(17)
