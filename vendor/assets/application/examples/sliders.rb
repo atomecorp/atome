6d684489fb6506c79ab489e67a6f11761d82a452
Jean-Eric Godard <jeezs@jeezs.net>
2024-03-22 09:04:25 +0100

    better sliders example

--- vendor/assets/application/examples/sliders.rb
+++ vendor/assets/application/examples/sliders.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
+
 label = text({ data: 0, top: 400, left: 69, component: { size: 12 }, color: :gray })
 
-grab(:intuition).slider({ id: :toto, range: { color: :white }, min: -12, max: 33, width: 333, value: 12, height: 25, left: 99, top: 350, color: :red, cursor: { color: :red, width: 25, height: 25 } }) do |value|
+aaa = grab(:intuition).slider({ id: :toto, range: { color: :yellow }, min: -12, max: 33, width: 333, value: 12, height: 25, left: 99, top: 350, color: :orange, cursor: { color: :orange, width: 25, height: 25 } }) do |value|
   label.data("(#{value})")
 end
 
-aa = grab(:intuition).slider({ orientation: :vertical, range: { color: :orange }, value: 55, width: 55, height: 555, attach: :intuition, left: 555, top: 33, color: :orange, cursor: { color: :orange, width: 33, height: 66, smooth: 3 } }) do |value|
+aa = grab(:intuition).slider({ orientation: :vertical, range: { color: :white }, value: 55, width: 55, height: 555, attach: :intuition, left: 555, top: 33, color: :red, cursor: { color: {alpha: 1, red: 0.12, green: 0.12, blue: 0.12}, width: 33, height: 66, smooth: 3 } }) do |value|
   label.data("(#{value})")
 end
 
-wait 2 do
-  aa.holder.value(33)
+b=box
+b.touch(true) do
+  aa.value(12)
+  aaa.value(-6)
 end
