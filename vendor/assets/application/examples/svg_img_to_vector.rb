a5722a54557e7bbbac7ed81e6122b460eab6dd3e
Jean-Eric Godard <jeezs@jeezs.net>
2024-03-30 18:24:27 +0100

    new svg_img_to_vector + example

--- vendor/assets/application/examples/svg_img_to_vector.rb
+++ vendor/assets/application/examples/svg_img_to_vector.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-image({ path: 'medias/images/icons/color.svg', id: :atomic_logo, width: 33, left: 333 })
+grab(:black_matter).image({ path: 'medias/images/icons/color.svg', id: :atomic_logo, width: 33, left: 333 })
 vector({ width: 333, height: 333, id: :my_placeholder })
 A.fetch_svg({ source: :atomic_logo, target: :my_placeholder })
+grab(:atomic_logo).delete(true)
+
 
