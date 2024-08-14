3b18fc2cf0d6677bccbc3d8010b8e397b1597558
Jean-Eric Godard <jeezs@jeezs.net>
2024-08-14 19:49:51 +0200

    vr integration almost complete except callback  version updated : '0.5.7.5.7'

--- vendor/assets/application/examples/vr.rb
+++ vendor/assets/application/examples/vr.rb
@@ -1,14 +1,2 @@
-vr({width: 1200,path: 'https://aframe.io/aframe/examples/boilerplate/panorama/puydesancy.jpg', id: :tutu})
+vr({width: 700,height: 390,path: 'medias/images/puydesancy.jpg', id: :tutu})
 
-# b=box({id: :container, width: 666, height: 333, color: :red})
-# js_code = <<~JAVASCRIPT
-#   initWithParam("https://aframe.io/aframe/examples/boilerplate/panorama/puydesancy.jpg", 'container', 'method', 'params');
-# JAVASCRIPT
-#
-#   JS.eval(js_code)
-#
-# wait 3 do
-#   b.width(444)
-#   # b.refresh
-# end
-# image({path: 'https://aframe.io/aframe/examples/boilerplate/panorama/puydesancy.jpg'})
