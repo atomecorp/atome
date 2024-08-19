031967a2a728b06195c73a1babe00d27fa92c3e8
Jean-Eric Godard <jeezs@jeezs.net>
2024-08-19 14:20:47 +0200

    starting calendar integration starting calendar inspection tool 'keyboard' particle is converted to new format

--- vendor/assets/application/examples/overflow.rb
+++ vendor/assets/application/examples/overflow.rb
@@ -1,30 +1,26 @@
 # frozen_string_literal: true
 
 b = box({ id: :the_container, width: 300, height: 300 })
-b.box({top: 500, color: :red})
-cc=b.circle({ top: 160, id: :the_circle })
+b.box({ top: 500, color: :red })
+cc = b.circle({ top: 160, id: :the_circle })
 
-initial_height=cc.height
-initial_width=cc.width
+initial_height = cc.height
+initial_width = cc.width
 b.overflow(:scroll) do |event|
   new_height = initial_height + event[:top]
   cc.height(new_height)
-  {left: event[:top]}
+  { left: event[:top] }
 end
-c=circle({ top: 370, color: :red})
+c = circle({ top: 370, color: :red })
 c.touch(:up) do
   b.overflow(:remove)
   c.delete(true)
-  c=circle({ top: 370,left: 90, color: :green})
+  c = circle({ top: 370, left: 90, color: :green })
   c.touch(true) do
     b.overflow(:scroll) do |event|
       puts 'removed!!'
       new_width = initial_width + event[:top]
       cc.width(new_width)
-      # {left: event[:top]}
-      # new_height = initial_height + event[:top]
-      # cc.height(new_height)
-      # {left: event[:top]}
     end
   end
 end
