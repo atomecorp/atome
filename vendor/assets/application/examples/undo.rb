d4b86c5e71b3ca39a53e1715cc60aff38c6b8d60
Jean-Eric Godard <jeezs@jeezs.net>
2025-01-25 15:26:08 +0100

    undo and redo works

--- vendor/assets/application/examples/undo.rb
+++ vendor/assets/application/examples/undo.rb
@@ -5,14 +5,86 @@ c = circle
 JS.eval('localStorage.clear();')
 Universe.allow_localstorage = true
 b.left(0)
-c.left(0)
+c.left(777)
 b.left(188)
 c.left(33)
-c.left(67)
+c.left(467)
 c.left(69)
 c.left(99)
 b.left(33)
 d = text({ data: :hello, left: 444 })
+
+
+
+##########################
+Universe.allow_localstorage = false
+@back_nb = 0
+
+c.touch(true) do
+  Universe.allow_localstorage = false
+  if @back_nb == 0
+    @back_nb = -1
+  end
+  new_back_nb = @back_nb - 1
+  excecuted = undo(new_back_nb, c.aid)
+
+  if excecuted == false
+    puts "locked : #{excecuted}"
+    "---------------------- No -----------------------"
+  else
+    puts "executed : #{excecuted}"
+    @back_nb = new_back_nb
+  end
+  Universe.allow_localstorage = true
+end
+
+b.touch(true) do
+  Universe.allow_localstorage = false
+  new_back_nb = @back_nb + 1
+  excecuted = undo(new_back_nb, c.aid)
+
+  if excecuted == false
+    puts "locked : #{excecuted}"
+    "---------------------- No -----------------------"
+  else
+    puts "executed : #{excecuted}"
+    @back_nb = new_back_nb
+  end
+  Universe.allow_localstorage = true
+end
+
+# c.touch(true) do
+#   Universe.allow_localstorage = false
+#   @back_nb = @back_nb  -1
+#   excecuted=undo(@back_nb, c.aid) # undo only history on c if on all objet :  undo(@back_nb)
+#   if excecuted == false
+#     @back_nb = @back_nb  +1
+#     puts "locked : #{excecuted}"
+#     "---------------------- No -----------------------"
+#   else
+#     puts "excecuted : #{excecuted}"
+#     @back_nb = @back_nb - 1
+#   end
+#   Universe.allow_localstorage = true
+# end
+#
+# b.touch(true) do
+#   Universe.allow_localstorage = false
+#   @back_nb = @back_nb + 1
+#   # undo(@back_nb, c.aid)
+#   excecuted=undo(@back_nb, c.aid) # undo only history on c if on all objet :  undo(@back_nb)
+#   if excecuted == false
+#     @back_nb = @back_nb  +1
+#     puts "locked : #{excecuted}"
+#     "---------------------- No -----------------------"
+#   else
+#     puts "excecuted : #{excecuted}"
+#     @back_nb = @back_nb + 1
+#   end
+#   Universe.allow_localstorage = true
+# end
+Universe.allow_localstorage = true
+##########################
 
 def extract_key_and_write(hash)
   first_level_key = hash.keys.first
@@ -35,7 +107,8 @@ def filter_hash_by_key(original_hash, target_key)
 end
 
 def undo(nb_of_backs, atome_to_target=nil)
-  nb_of_backs = nb_of_backs - 2
+
+  # nb_of_backs = nb_of_backs - 2
   a = grab(:view)
   if atome_to_target
     history_to_treat=filter_hash_by_key(a.history, atome_to_target)
@@ -46,28 +119,24 @@ def undo(nb_of_backs, atome_to_target=nil)
 
   key_found = history_to_treat.keys[history_to_treat.length + nb_of_backs]
   value_found = history_to_treat[key_found]
-  if history_to_treat.length + nb_of_backs <= (history_to_treat.length - 1)
+  puts "==> #{history_to_treat.length + nb_of_backs }"
+  if history_to_treat.length + nb_of_backs <= (history_to_treat.length - 1) &&  history_to_treat.length + nb_of_backs >= 0  && nb_of_backs <= history_to_treat.length
+    puts 'exec'
     prev_action = extract_key_and_write(value_found)
     prev_action.each do |aid_f, action|
       action.each do |prop_f, val_fw|
         hook(aid_f).send(prop_f, val_fw)
       end
     end
+    nb_of_backs # important or it crash
+  else
+    false # important or it crash
   end
-  nb_of_backs # important or it crash
 end
 
-@back_nb = 0
-c.touch(true) do
-  undo(@back_nb, c.aid) # undo only history on c if on all objet :  undo(@back_nb)
-  @back_nb = @back_nb - 2
-end
 
-b.touch(true) do
-  undo(@back_nb)
-  @back_nb = @back_nb + 2
-end
 
-d.touch(true) do
-  puts 'kkkooo'
+
+wait 1 do
+  JS.eval('console.clear()')
 end
